b869065a6ec9e867e9acd389457bf31af0cf5a23
uBrunoow <brunowerner7@gmail.com>
2024-09-29 14:58:37 -0300

    :feat: adiciona um forms de contato

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -3,9 +3,70 @@ import { useState } from 'react'
 import SectionTitle from '../Common/SectionTitle'
 import OfferList from './OfferList'
 import PricingBox from './PricingBox'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Input } from '../ui/input'
+
+
+import { google } from 'googleapis';
+import Link from 'next/link'
+
+
+// AKfycbzTW1tpoR2SBNSe7LJiMITGEbJ3CZWvA9FldEncS0C7i1J_wl-U5A1LuqbhyPvE8Olcfw
+// https://script.google.com/macros/s/AKfycbzTW1tpoR2SBNSe7LJiMITGEbJ3CZWvA9FldEncS0C7i1J_wl-U5A1LuqbhyPvE8Olcfw/exec
+
+// async function handler(req, res) {
+//   if (req.method === 'POST') {
+//     const { nome, email } = req.body;
+
+//     const auth = new google.auth.GoogleAuth({
+//       credentials: {
+//         client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//         private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//       },
+//       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+//     });
+
+//     const sheets = google.sheets({ version: 'v4', auth });
+
+//     const spreadsheetId = process.env.SPREADSHEET_ID;
+//     const range = 'Sheet1!A:C'; // Ajuste o intervalo conforme necessário
+
+//     await sheets.spreadsheets.values.append({
+//       spreadsheetId,
+//       range,
+//       valueInputOption: 'RAW',
+//       requestBody: {
+//         values: [[nome, email]],
+//       },
+//     });
+
+//     res.status(200).json({ message: 'Dados enviados com sucesso!' });
+//   } else {
+//     res.status(405).json({ message: 'Método não permitido' });
+//   }
+// }
 
 const Pricing = () => {
   const [isMonthly, setIsMonthly] = useState(true)
+  const [formData, setFormData] = useState({ nome: '', email: ''})
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target
+  //   setFormData((prevData) => ({ ...prevData, [name]: value }))
+  // }
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //   const response = await fetch('https://script.google.com/macros/s/AKfycbzTW1tpoR2SBNSe7LJiMITGEbJ3CZWvA9FldEncS0C7i1J_wl-U5A1LuqbhyPvE8Olcfw/exec', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(formData),
+  //   })
+  //   const result = await response.json()
+  //   alert(result.message)
+  // }
 
   return (
     <section id="pricing" className="relative z-10 py-16 md:py-20 lg:py-28">
@@ -58,20 +119,6 @@ const Pricing = () => {
         </div>
 
         <div className=" gap-x-8 gap-y-10 flex items-center justify-center">
-          {/* <PricingBox
-            packageName="Lite"
-            price={isMonthly ? '0' : '0'}
-            duration={isMonthly ? 'mês' : 'ano'}
-            subtitle="Experimente nosso plano Lite gratuitamente e descubra como a gamificação pode transformar seu aprendizado."
-          >
-            <OfferList text="Acesso a Módulos Básicos de TI" status="active" />
-            <OfferList text="Certificados Limitados" status="active" />
-            <OfferList text="Desafios de Nível Básico" status="active" />
-            <OfferList text="Suporte por Email" status="active" />
-            <OfferList text="Sem Acesso a Competição entre Alunos" status="inactive" />
-            <OfferList text="Acesso a Comunidade Exclusiva" status="inactive" />
-          </PricingBox> */}
-          
           <PricingBox
             packageName="Basic"
             price={isMonthly ? '49,90' : '525,88'}
@@ -85,24 +132,50 @@ const Pricing = () => {
             <OfferList text="Acesso a Competição entre Alunos" status="active" />
             <OfferList text="Acesso a Comunidade Exclusiva" status="inactive" />
           </PricingBox>
-          
-          {/* <PricingBox
-            packageName="Plus"
-            price={isMonthly ? '199,90' : '2398,80'}
-            duration={isMonthly ? 'mês' : 'ano'}
-            subtitle="Desbloqueie todos os recursos com o plano Plus e acelere seu aprendizado com máxima gamificação."
-          >
-            <OfferList text="Acesso a Todos os Módulos Avançados de TI" status="active" />
-            <OfferList text="Certificados Ilimitados e Personalizados" status="active" />
-            <OfferList text="Desafios de Nível Avançado" status="active" />
-            <OfferList text="Participação em Rankings Nacionais" status="active" />
-            <OfferList text="Acesso a Competição entre Alunos e Universidades" status="active" />
-            <OfferList text="Acesso a Comunidade Exclusiva com Mentorias" status="active" />
-          </PricingBox> */}
         </div>
+      </div>
 
+      <div className='pt-[150px] px-[100px] flex items-center justify-center flex-col'>
+        {/* <SectionTitle
+          title="Entre em contato conosco"
+          paragraph="Faça seu orçamento personalizado inserindo o seu nome e o seu e-mail e entraremos em contato."
+          center
+          width="665px"
+        /> */}
 
+<h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
+Entre em contato conosco
+        </h2>
+        <p className="text-base !leading-relaxed text-body-color md:text-lg">
+        Faça seu orçamento personalizado inserindo o seu nome e o seu e-mail e entraremos em contato.        </p>
 
+        <form className="flex w-full flex-col items-center justify-center gap-5">
+          {/* <div className="w-full max-w-lg">
+            <label>Nome</label>
+            <Input
+              type="text"
+              name="nome"
+              value={formData.nome}
+              // onChange={handleChange}
+              className="h-[45px] border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+              placeholder="Digite seu nome"
+            />
+          </div>
+          <div className="w-full max-w-lg">
+            <label>Email</label>
+            <Input
+              type="email"
+              name="email"
+              value={formData.email}
+              // onChange={handleChange}
+              className="h-[45px] border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+              placeholder="Digite seu email"
+            />
+          </div> */}
+          <Link href={"https://forms.gle/h72rMzBwdYF9wMfj7"}  target="_blank" className="mt-4 h-[45px] rounded text-center bg-primary w-full max-w-lg px-4 py-2 text-white">
+            Entrar em contato
+          </Link>
+        </form>
       </div>
 
       <div className="absolute bottom-0 left-0 z-[-1]">
